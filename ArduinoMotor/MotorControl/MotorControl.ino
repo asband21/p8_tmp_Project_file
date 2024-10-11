48517fceef60ed8d7e3249d906fdf19c9f8276bb
paser001 <pa.serra001@gmail.com>
2024-10-11 08:54:31 +0200

    Create backwards and forwards functions

--- ArduinoMotor/MotorControl/MotorControl.ino
+++ ArduinoMotor/MotorControl/MotorControl.ino
@@ -1,42 +1,52 @@
-int pwmPin = 2;  // Choose a PWM-capable pin (9 in this case)
+int pwmPin = 2;
+
+
 
 void setup() {
-  // Start serial communication
   Serial.begin(9600);
-  
-  // Set PWM pin as output
   pinMode(pwmPin, OUTPUT);
-  
-  // Print instructions
-  Serial.println("Type a value between 0 and 255 to set the PWM signal.");
+  analogWrite(pwmPin, 128);
+  Serial.println("Type a value between 0 to 1960");
 }
 
 void loop() {
-  // Check if any serial data is available
   if (Serial.available() > 0) {
-    // Read the input as a string
     String input = Serial.readStringUntil('\n');
+    int rotation = input.toInt();
     
-    // Convert the input to an integer
-    int pwmValue = input.toInt();
+    movefwd(rotation);
     
-    // Ensure the value is between 0 and 255
-    if (pwmValue >= 0 && pwmValue <= 255) {
-      // Set the PWM output to the typed value
-      analogWrite(pwmPin, pwmValue);
-      
-      // Provide feedback to the user
-      Serial.print("Setting PWM to: ");
-      Serial.println(pwmValue);
-    } else {
-      Serial.println("Please enter a value between 0 and 255.");
-    }
+    
+    
+    // if (pwmValue >= 0 && pwmValue <= 255) {
+    //   analogWrite(pwmPin, pwmValue);
+    //   Serial.print("Setting PWM to: ");
+    //   Serial.println(pwmValue);
+    // } else {
+    //   Serial.println("Wrong value");
+    // }
   }
 }
 
+void movefwd(int rpm){
+  uint8_t pwm_value= map(rpm, 0, 1963, 159, 255);
+  if(pwm_value<168){
+    analogWrite(pwmPin, 168);
+    //delay
+  }
+  analogWrite(pwmPin, pwm_value);
+}
 
-// 159 Top to bottom forward cutout
-// 167 Bottom to top forward cutout
+void movebwd(int rpm){
+  uint8_t pwm_value= map(rpm, 0, 1972, 78, 46);
+  if(pwm_value>80){
+    analogWrite(pwmPin, 80);
+    //delay
+  }
+  analogWrite(pwmPin, pwm_value);
+}
+// // // // // 159 Top to bottom forward cutout
+// // // // // 167 Bottom to top forward cutout
 
 
 // 78 Top to bottom backward cutout
