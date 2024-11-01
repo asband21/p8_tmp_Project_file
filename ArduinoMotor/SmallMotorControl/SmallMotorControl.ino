d732cd8aec6ef500d8408d2a24bd6105b8a75dcc
paser001 <pa.serra001@gmail.com>
2024-11-01 13:37:15 +0100

    Add rotation to menu

--- ArduinoMotor/SmallMotorControl/SmallMotorControl.ino
+++ ArduinoMotor/SmallMotorControl/SmallMotorControl.ino
@@ -1,13 +1,29 @@
-const int IN1 = 4;  // PWM for Motor 1
-const int IN2 = 10;  // Direction for Motor 1
-const int IN3 = 13;  // PWM for Motor 2
-const int IN4 = 12;  // Direction for Motor 2
+const int IN1 = 13;  // PWM for Motor 1        //BEING USED FOR STOP PIN
+const int IN2 = 12;  // Direction for Motor 1
+const int IN3 = 8;  // PWM for Motor 2
+const int IN4 = 10;  // Direction for Motor 2
+
+#define ENCODER_A_PIN 2  // Channel A
+#define ENCODER_B_PIN 3  // Channel B
+#define STOP_PIN 4 // Stop pin for testing 
+#define CW_PIN 5 // Clockwise motor pin
+#define CCW_PIN 6 // Counter-clockwise motor pin
+#define ALLOWEDERROR 2
 
 int pwmValue1 = 0;  // PWM value for Motor 1
 int pwmValue2 = 0;  // PWM value for Motor 2
 bool inMenu = true;
 bool inManualMode = false;
 bool inAutoMode = false;
+
+
+
+volatile long positionCount = 0;
+int lastAState = LOW;
+const float ppr = 10347;  // Pulses per revolution of your encoder
+float degreesPerPulse = (float)ppr / 180;
+float goalAngle = 0;
+
 
 void setup() {
   pinMode(IN1, OUTPUT);
@@ -19,6 +35,18 @@ void setup() {
   analogWrite(IN3, 0);
   digitalWrite(IN2, LOW);
   digitalWrite(IN4, HIGH);
+
+  pinMode(ENCODER_A_PIN, INPUT);
+  pinMode(ENCODER_B_PIN, INPUT);
+  pinMode(CCW_PIN, OUTPUT);
+  pinMode(CW_PIN, OUTPUT);
+  pinMode(STOP_PIN, OUTPUT);
+  digitalWrite(CW_PIN, LOW);
+  digitalWrite(CCW_PIN, LOW);
+  digitalWrite(STOP_PIN, LOW);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A_PIN), updatePosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_B_PIN), updatePosition, CHANGE);
+
   printMenu();
 }
 
@@ -36,11 +64,29 @@ void loop() {
       inAutoMode = true;
       inManualMode = false;
       Serial.println("Automatic mode selected. Type 'exit' to return to menu.");
-    } else if (input == "exit") {
-      inManualMode = false;
-      inAutoMode = false;
-      inMenu = true;
-      printMenu();
+    } else if (input == "r") {
+        inManualMode = false;
+        inAutoMode = false;
+        Serial.println("Enter an angle for refTracking (e.g., 45):");
+        while (Serial.available() == 0); 
+        String angleInput = Serial.readStringUntil('\n');
+        angleInput.trim();
+        float angleInp = angleInput.toFloat();
+        float currentAngle = fmod((positionCount * degreesPerPulse + 180),(360 - 180));
+        goalAngle = fmod((angleInp+180), (360 - 180));
+        float deltaRot= getDeltaRotation(currentAngle, goalAngle);
+        Serial.print("Moving to angle: ");
+        Serial.println(goalAngle);
+        Serial.print("Current angle: ");
+        Serial.println(currentAngle);
+        Serial.print("Delta rot: ");
+        Serial.println(deltaRot);
+        refTracking(deltaRot);
+     } else if (input == "exit") {
+        inManualMode = false;
+        inAutoMode = false;
+        inMenu = true;
+        printMenu();
     } else {
       Serial.println("Invalid input. Please select 'm' for manual or 'a' for automatic.");
     }
@@ -108,8 +154,63 @@ void manualMode() {
 
 void printMenu() {
   Serial.println("====== Motor Control Menu ======");
-  Serial.println("Type 'm' for manual mode.");
-  Serial.println("Type 'a' for automatic mode.");
-  Serial.println("Type 'exit' to return to this menu.");
+  Serial.println("'m' for manual mode.");
+  Serial.println("'a' for automatic mode.");
+  Serial.println("'r' to input an angle for refTracking.");
+  Serial.println("'exit' to return to this menu.");
   inMenu = true;
+}
+
+
+float getDeltaRotation(float currentAngle, float goalAngle) { //Chatgpt did this one but if you want you can debug yours
+  float delta = goalAngle - currentAngle;
+  if (delta > 180) {
+    delta -= 360; 
+  } else if (delta < -180) {
+    delta += 360;  
+  }
+  return delta;
+}
+
+
+void updatePosition() {
+  int aState = digitalRead(ENCODER_A_PIN);
+  int bState = digitalRead(ENCODER_B_PIN);
+
+ 
+  if (aState != lastAState) {
+    if (aState == HIGH && bState == LOW || aState == LOW && bState == HIGH){
+      positionCount--; // Clockwise
+    } else {
+      positionCount++; // Counter-clockwise
+    }
+  }
+  lastAState = aState;
+}
+
+
+void refTracking(float deltaRotation) {
+  long int goalPositionCount = positionCount + deltaRotation*ppr/360;
+  long int allowedTrackingError= ppr*ALLOWEDERROR/360;
+
+  Serial.println(goalPositionCount);
+  Serial.println(allowedTrackingError);
+  while (!(positionCount > goalPositionCount - allowedTrackingError && positionCount <= goalPositionCount + allowedTrackingError)) { 
+    if (positionCount < goalPositionCount) {
+      digitalWrite(STOP_PIN, LOW);
+      digitalWrite(CW_PIN, LOW);
+      digitalWrite(CCW_PIN, HIGH);
+      Serial.println(positionCount);
+    }
+    if (positionCount > goalPositionCount) {
+      digitalWrite(STOP_PIN, LOW);
+      digitalWrite(CCW_PIN, LOW);
+      digitalWrite(CW_PIN, HIGH);
+      Serial.println(positionCount);
+    }
+    delay(10);
+  }
+  digitalWrite(CW_PIN, LOW);
+  digitalWrite(CCW_PIN, LOW);
+  digitalWrite(STOP_PIN, HIGH);
 }
