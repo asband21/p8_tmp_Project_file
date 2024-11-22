a98bae4333b81d0eae93efd25f2548533fe43b67
Asbjørn <kykutura@gmx.com>
2024-11-22 15:51:51 +0100

    små timg

--- Simulink/get_parameters.m
+++ Simulink/get_parameters.m
@@ -8,9 +8,7 @@ moter_left  = [-1, -3, 0, 0];
 moter_right = [ 1, -3, 0, 0];
 moter_bow   = [ 0,  2, 0, 0];
 
-n_l = norm(moter_left)
-n_r = norm(moter_right)
-n_b = norm(moter_bow)
+
 
 % calculating the reference of the angle.
 moter_left(3)  = atan2( moter_left(2), moter_left(1) );
@@ -21,6 +19,9 @@ moter_bow(3)   = atan2(  moter_bow(2), moter_bow(1)  );
 moter_left(4)  = sqrt(moter_left(1)^2  + moter_left(2)^2 )
 moter_right(4) = sqrt(moter_right(1)^2 + moter_right(2)^2)
 moter_bow(4)   = sqrt(moter_bow(1)^2   + moter_bow(2)^2  )
+%moter_left(4) = norm(moter_left);
+%moter_right(4) = norm(moter_right);
+%moter_bow(4) = norm(moter_bow);
 
 
 % x axet/width 2.74 m;
@@ -48,7 +49,7 @@ friction = [b_x, b_y, b_t];
 
 A = [b_x/mass 0        0        ;
      0        b_y/mass 0        ;
-     0        0        b_t/mass];
+     0        0        b_t/inertia];
 
 wind_gain = 2;
 
