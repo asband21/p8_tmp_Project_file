b264bcfbbd4ba07c22b60d654e307ab64d64795b
Asbjørn <kykutura@gmx.com>
2024-11-25 15:55:47 +0100

    Revert "Merge branch 'master' of github.com:asband21/P751"
    reverses that merge so we get all the Simulink files back and some of the Arino files back.
    
    This reverts commit b957ccd80d4b6e8d48a0103e9517282a4e8da649, reversing
    changes made to 85b9b5798d7d9541b2d574e363270a318efdcdeb.

---
+++ Simulink/old_and_test/calculateLQR.m
@@ -0,0 +1,67 @@
+function [theta_right, theta_left, K,S,P] = calculateLQR()
+
+bx = -100;
+by = -1000;
+br = -2000;
+m = 3000;
+len = 10;
+I = (1/12)*m*len*len;
+%x = [x_d; y_d; theta_d];
+
+A = [bx/m 0 0;
+     0 by/m 0;
+     0 0 br/m]
+
+%theta_1 = 1;
+moter_1_x = -2;
+moter_1_y = -5;
+len_origen_1 = sqrt(moter_1_y*moter_1_y + moter_1_x*moter_1_x);
+vector_angel_1 = atan2(moter_1_y, moter_1_y);
+
+%theta_2 = 1;
+moter_2_x = 2;
+moter_2_y = -5;
+len_origen_2 = sqrt(moter_1_y*moter_1_y + moter_1_x*moter_1_x);
+vector_angel_2 = atan2(moter_1_y, moter_1_y);
+
+theta_3 = pi/2; 
+moter_1_3 = 0;
+moter_1_3 = 4;
+len_origen_3 = sqrt(moter_1_y*moter_1_y + moter_1_x*moter_1_x);
+vector_angel_3 = atan2(moter_1_y, moter_1_y);
+
+
+b_min = @(theta) cond([sin(theta(1))*len_origen_1  , sin(theta(2))*len_origen_2  , sin(theta_3)*len_origen_3;
+                       cos(theta(1)+vector_angel_1), cos(theta(2)+vector_angel_2), 0;
+                       sin(theta(1)+vector_angel_1), sin(theta(2)+vector_angel_2), 1])
+
+tt0 = b_min([0,0])
+initial_guess = [0.3, 1.23];
+% men graf over optimering;
+%options = optimset('PlotFcns',@optimplotfval);
+minimum = fminsearch(b_min, initial_guess) %,options)
+theta_left  = minimum(1);
+theta_right = minimum(2);
+
+
+
+
+
+B = [sin(theta_left)*len_origen_1  , sin(theta_right)*len_origen_2  , sin(theta_3)*len_origen_3;
+     cos(theta_left+vector_angel_1), cos(theta_right+vector_angel_2), 0;
+     sin(theta_left+vector_angel_1), sin(theta_right+vector_angel_2), 1]
+
+con = cond(B)
+
+
+Q = [100 0 0;
+     0 100 0;
+     0 0 100];
+
+R = [1 0 0;
+     0 1 0;
+     0 0 1];
+
+[K,S,P] = lqr(A,B,Q,R)
+
+end
---
+++ Simulink/re_LQR.m
@@ -0,0 +1,43 @@
+function [K, B, thuster_angle] = re_LQR()
+
+%loading, and model parameters
+[moter_left, moter_right, moter_bow, A ,mass, boat_dimensions, friction, inertia, wind_gain] = get_parameters()
+
+%x_d = A*x + B*u;
+%x = [x, y , angle]
+%u = [moter_left, moter_right, moter_bow]
+
+%setting up the functions for optimization.
+%theta = [moter_left, moter_right]
+B_fuc = @(theta) [cos(theta(1)),                             cos(theta(2))                               ,1                              ;
+                  sin(theta(1)),                             sin(theta(2))                               ,0                              ;
+                  sin(moter_left(3)+theta(1))*moter_left(4), sin(moter_right(3)+theta(2))*moter_right(4) ,sin(moter_bow(3))*moter_bow(4)];
+
+min_b = @(theta) cond(B_fuc(theta));
+
+
+% Starting optimization process
+initial_guess = [0.3, 1.23];
+
+%show a graph over the optimization uncommon the following line.
+%options = optimset('PlotFcns',@optimplotfval);
+
+thuster_angle = fminsearch(min_b, initial_guess) %,options)
+
+B   = B_fuc(thuster_angle)
+cot = min_b(thuster_angle)
+
+thuster_angle = [thuster_angle, 0]
+
+Q = [2 0 0   ;
+     0 10 0  ;
+     0 0 20];
+%Q = [2 0 0 ;0 2 0 ;0 0 2];
+Q = Q*10
+
+R = [1 0 0;
+     0 1 0;
+     0 0 1];
+
+[K,S,P] = lqr(A,B,Q,R)
+end
