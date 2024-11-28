d252ea50143d59381c2870fe3e82b006b4c1612e
Asbjørn <kykutura@gmx.com>
2024-11-28 17:26:57 +0100

    I want to feed both wireless and some noise and parameters in forcing for the  ground simulink model.

--- wildest_apc_220/receiver/receiver.ino
+++ wildest_apc_220/receiver/receiver.ino
@@ -1,6 +1,6 @@
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 
-SoftwareSerial apc220Serial(14, 15);
+//SoftwareSerial apc220Serial(14, 15);
 
 const int ledPin = LED_BUILTIN; // Pin for the onboard LED
 int blinkRate = 1;             // Default blink rate in Hz
@@ -11,22 +11,29 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, ledState);
   Serial.begin(9600); // Start serial communication at 9600 baud
-  Serial.println();
   
+  /*
+  Serial.println();
   apc220Serial.begin(9600);
+  while (!apc220Serial){}
+  Serial.println("der er tid");
+  apc220Serial.println();
+  */
   //while (!apc220Serial){}
 }
 
 void loop() {
-    if (apc220Serial.available() > 0) {
-    char received = apc220Serial.read();
-    Serial.println(received);
-    if (received >= '1' && received <= '9')
-    {
-      blinkRate = received - '0';
+    if (Serial.available() > 0) {
+      
+      char received = Serial.read();
+      /*
+      Serial.print("read:");
+      Serial.println(received);
+      apc220Serial.println();
+      */
+      if (received >= '1' && received <= '9')
+            blinkRate = received - '0';
     }
-    apc220Serial.println();
-  //}
 
   unsigned long interval = 1000 / blinkRate;
 
