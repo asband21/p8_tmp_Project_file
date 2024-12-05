c73e55a5e519931cd5519be41331ad38508613a6
asbjørn <qw41ha@student.aau.dk>
2024-12-05 22:34:51 +0100

    a bit further on the caramel filter, now updating the covariance mean.

--- Simulink/kalman_filter_sim.m
+++ Simulink/kalman_filter_sim.m
@@ -1,33 +1,34 @@
-function [x_hat] = kalman_filter_sim_l(time, olde_time, x_mes, x_hat_old)
-td = time - olde_time; %time delta
-
-bc = -1000;
-m = 1000;
-
-A = [0, 1,    0 ;
-     0, bc/m, 0 ;
-     0, 0,    0];
-
-B = [0, 0, 0;
-     0, 1, 0;
-     0, 0, 0];
-
-C = [1, 0, 0;
-     0, 1, 0;
-     0, 0, 1];
-
-Q = diag([1e-5, 1e-5, 1e-7]);  % Process noise covariance
-R = 1e-2;                      % Measurement noise covariance
-
-x_hat = A*x_hat_old*td+x_hat_old;
+function [x_hat,P] = kalman_filter_sim_l(time, olde_time,P_m1, x_mes, x_hat_old)
+    td = time - olde_time; %time delta
+    
+    bc = -1000;
+    m = 1000;
+    
+    A = [0, 1,    0 ;
+         0, bc/m, 0 ;
+         0, 0,    0];
+    
+    B = [0, 0, 0;
+         0, 1, 0;
+         0, 0, 0];
+    
+    C = [1, 0, 0;
+         0, 1, 0;
+         0, 0, 1];
+    
+    Q = diag([1e-5, 1e-5, 1e-7]);  % Process noise covariance
+    R = 1e-2;                      % Measurement noise covariance
+    
+    I = eye(3)
+    P = (I-k*H)*P_m1
+    %x_hat = A*x_hat_old*td+x_hat_old;
 end
 
 function [x_1] = dynamik(time, olde_time, x)
-td = time - olde_time; %time delta
-A = [0, 1,    0 ; 0, -1200/1500, 0 ;0, 0,    0];
-x_1 = A*x*td+x;
+    td = time - olde_time; %time delta
+    A = [0, 1,    0 ; 0, -1200/1500, 0 ;0, 0,    0];
+    x_1 = A*x*td+x;
 end
-
 
 N = 100;
 time = zeros(1, N);
@@ -37,14 +38,17 @@ time = ts:ts:N*ts;
 x = zeros(3, N);        % True system dynamics.
 x(:, 1) = [0; 1; 0];    % Initial state
 x_hat = zeros(3, N);
-
+x_hat(:, 1) = [0; 1; 0];
+p = [1 0 0;
+     0 1 0;
+     0 0 1;] %estimate error covariance
 
 u = zeros(1, N);        % Zero input force
 u(100:200) = 10; 
 
 for i = 2:N
     x(:,i)      = dynamik(time(i), time(i-1), x(:,i-1)); % True system dynamics.
-    x_hat(:,i)  = kalman_filter_sim_l(time(i), time(i-1), x(:,i-1)+random('Normal',0,1), x_hat(:,i-1)); % Estes Medt state.
+    x_hat(:,i)  = kalman_filter_sim_l(time(i), time(i-1),p , x(:,i-1)+random('Normal',0,1), x_hat(:,i-1)); % Estes Medt state.
 end
 
 
